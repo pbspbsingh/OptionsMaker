43871c3d2c1ba41d43bf50168a886915543a8a1d
Prashant Singh <singhpra@meta.com>
2025-06-26 21:06:54 -0700

    Test client: Updated options quotes

--- options_maker_server/broker/client_test.py
+++ options_maker_server/broker/client_test.py
@@ -3,10 +3,13 @@ import logging
 import os
 import sys
 
+from schwab.orders.options import OptionSymbol
+
 import broker
 
-# OPTION_SYMBOL = "META 250627C00677500"
-OPTION_SYMBOL = "AAPL 250620C00200000"
+
+OPTION_SYMBOL = "META  250627C00677500"
+
 
 
 async def main():
@@ -15,13 +18,18 @@ async def main():
     client = await broker._init()
     schwab_client = client._client
 
-    res = await schwab_client.get_quotes(symbols=[OPTION_SYMBOL])
+    opt = OptionSymbol("META", "250627", "C", "677.5")
+    opt_symbol = opt.build()
+    opt_symbol = OPTION_SYMBOL
+
+
+    res = await schwab_client.get_quotes(symbols=[opt_symbol])
     res.raise_for_status()
     print(res.json())
 
     client._stream_client.add_level_one_option_handler(lambda o: print(o["content"]))
-    await client._stream_client.level_one_option_subs([OPTION_SYMBOL])
-
+    await client._stream_client.level_one_option_subs([opt_symbol])
+    #
     await asyncio.sleep(30)
     print("Done")
 
